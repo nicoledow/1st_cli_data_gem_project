ffa096e6ae4be8e16774e52fdc250a90c8aababf
Nicole Dow <nikki.dow71@gmail.com>
2019-08-05 13:46:56 +0000

    first commit

---
+++ lib/cli_data_gem_project.rb
@@ -0,0 +1,6 @@
+require "cli_data_gem_project/version"
+
+module CliDataGemProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cli_data_gem_project/version.rb
@@ -0,0 +1,3 @@
+module CliDataGemProject
+  VERSION = "0.1.0"
+end
